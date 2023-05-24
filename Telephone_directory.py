4d3b5dd6b11145b2f5be12c3558e0db7a0039fc7
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-05-24 11:58:05 +0300

    Created def user_search

--- Telephone_directory.py
+++ Telephone_directory.py
@@ -1,22 +1,20 @@
-def user_imprt():
-    with open ("telephone_directory.txt", "a", encoding="utf-8") as file:
-        file.write(input("Введите Фамилию: ") + " ")
-        file.write(input("Введите Имя: ") + " ")
-        file.write(input("Введите Отчество: ") + " ")
-        file.write(str(int(input("Введите телефон: "))) + "\n")
-#
-#
-user_imprt()
+# def user_imprt():
+#     with open ("telephone_directory.txt", "a", encoding="utf-8") as file:
+#         file.write(input("Введите Фамилию: ") + " ")
+#         file.write(input("Введите Имя: ") + " ")
+#         file.write(input("Введите Отчество: ") + " ")
+#         file.write(str(int(input("Введите телефон: "))) + "\n")
+# #
+# #
+# user_imprt()
 
-# with open('telephone_directory.txt', 'r', encoding="utf-8") as file:
-#     words = input()
-#     line = file.readline().split()
-#     # some_line = file.readline()
-#     while line:
-#         for el in line:
-#             if el == words:
-#                 print(*line)
-#                 break
-#         line = file.readline().split()
-#         # some_line = file.readline()
+def user_search():
+    with open('telephone_directory.txt', 'r', encoding="utf-8") as file:
+        search_word = input()
+        line = file.readline().split()
+        while line:
+            for el in line:
+                if el == search_word:
+                    print(*line)
+            line = file.readline().split()
 
