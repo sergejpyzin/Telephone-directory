4ae6c0d27c3cbc5a6309535af3e8c4ef8c29e760
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-05-24 11:51:41 +0300

    Created def user_imprt

--- Telephone_directory.py
+++ Telephone_directory.py
@@ -1,16 +1,22 @@
-# def user_imprt():
-#     with open ("telephone_directory.txt", "a", encoding="utf-8") as file:
-#         file.write(input("Введите Фамилию: ") + "\t")
-#         file.write(input("Введите Имя: ") + "\t")
-#         file.write(input("Введите Отчество: ") + "\t")
-#         file.write(str(int(input("Введите телефон: "))) + "\n")
+def user_imprt():
+    with open ("telephone_directory.txt", "a", encoding="utf-8") as file:
+        file.write(input("Введите Фамилию: ") + " ")
+        file.write(input("Введите Имя: ") + " ")
+        file.write(input("Введите Отчество: ") + " ")
+        file.write(str(int(input("Введите телефон: "))) + "\n")
 #
 #
-# user_imprt()
+user_imprt()
 
-with open('telephone_directory.txt', 'r', encoding="utf-8") as file:
-    # words = input()
-    line = file.readline().split()
-    print(line)
-    for el in line:
-        print(el)
+# with open('telephone_directory.txt', 'r', encoding="utf-8") as file:
+#     words = input()
+#     line = file.readline().split()
+#     # some_line = file.readline()
+#     while line:
+#         for el in line:
+#             if el == words:
+#                 print(*line)
+#                 break
+#         line = file.readline().split()
+#         # some_line = file.readline()
+
